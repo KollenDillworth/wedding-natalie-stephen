f9c16aad44b05740a3fb22d88d535d90d1dff62b
Kollen Dillworth <kdillwor@sundevils.asu.edu>
2021-10-16 15:12:44 -0700

    more cards in travel

--- the-one/src/app/travel/travel.component.ts
+++ the-one/src/app/travel/travel.component.ts
@@ -1,7 +1,7 @@
 import { animate, AnimationBuilder, AnimationFactory, AnimationPlayer, style } from '@angular/animations';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RestaurantModel } from '../models/restaurant-model';
-import { americanRestuarantData, asianRestuarantData, restuarantData } from '../models/restuarant-data';
+import { americanRestuarantData, asianRestuarantData, healthyRestuarantData, italianRestuarantData, mexSeaRestuarantData, restuarantData } from '../models/restuarant-data';
 @Component({
   selector: 'app-travel',
   templateUrl: './travel.component.html',
@@ -12,11 +12,24 @@ export class TravelComponent implements OnInit {
   restaurants: RestaurantModel = restuarantData;
   americanRestaurants: RestaurantModel = americanRestuarantData;
   asianRestaurants: RestaurantModel = asianRestuarantData;
+  italianRestaurants: RestaurantModel = italianRestuarantData;
+  mexSeaRestaurants: RestaurantModel = mexSeaRestuarantData;
+  healthyRestaurants: RestaurantModel = healthyRestuarantData;
 
   private currentSlide = 0;
   private currentSlideSm = 0;
+
   private currentSlideA = 0;
+  private currentSlideASm = 0;
+
   private currentSlideAs = 0;
+  private currentSlideAsSm = 0;
+
+  private currentSlideItSm = 0;
+
+  private currentSlideMexSm = 0;
+
+  private currentSlideHealthySm = 0;
 
   @ViewChild('carousel')
   private carousel!: ElementRef<any>;
@@ -26,20 +39,33 @@ export class TravelComponent implements OnInit {
   private carouselSm!: ElementRef<any>;
   private playerSm!: AnimationPlayer;
 
-
-
-
   @ViewChild('carouselA')
   private carouselA!: ElementRef<any>;
   private playerA!: AnimationPlayer;
 
-
-
+  @ViewChild('carouselASM')
+  private carouselASM!: ElementRef<any>;
+  private playerASM!: AnimationPlayer;
 
   @ViewChild('carouselAs')
   private carouselAs!: ElementRef<any>;
   private playerAs!: AnimationPlayer;
   
+  @ViewChild('carouselAsSm')
+  private carouselAsSm!: ElementRef<any>;
+  private playerAsSm!: AnimationPlayer;
+  
+  @ViewChild('carouselItSm')
+  private carouselItSm!: ElementRef<any>;
+  private playerItSm!: AnimationPlayer;
+  
+  @ViewChild('carouselMexSm')
+  private carouselMexSm!: ElementRef<any>;
+  private playerMexSm!: AnimationPlayer;
+
+  @ViewChild('carouselHealthySm')
+  private carouselHealthySm!: ElementRef<any>;
+  private playerHealthySm!: AnimationPlayer;
 
 
 
@@ -140,6 +166,35 @@ export class TravelComponent implements OnInit {
     this.playerA.play();
   }
 
+  nextAsM(){
+    if(this.currentSlideASm + 1 === this.americanRestaurants.restaurants.length) return;
+      this.currentSlideASm = (this.currentSlideASm + 1) % this.americanRestaurants.restaurants.length;
+      
+      const offset = this.currentSlideASm * 315;
+
+      const myAnimation : AnimationFactory = this.builder.build([
+        animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+      ]);
+
+      this.playerASM = myAnimation.create(this.carouselASM.nativeElement);
+      this.playerASM.play();
+  }
+
+
+  prevAsM(){
+    if(this.currentSlideASm + 1 === 0 || this.currentSlideASm === 0) return;
+    this.currentSlideASm = ((this.currentSlideASm - 1) % this.americanRestaurants.restaurants.length) % this.americanRestaurants.restaurants.length;
+    
+    const offset = this.currentSlideASm * 315;
+
+    const myAnimation : AnimationFactory = this.builder.build([
+      animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+    ]);
+
+    this.playerASM = myAnimation.create(this.carouselASM.nativeElement);
+    this.playerASM.play();
+  }
+
 
 
 
@@ -170,6 +225,123 @@ export class TravelComponent implements OnInit {
 
     this.playerAs = myAnimation.create(this.carouselAs.nativeElement);
     this.playerAs.play();
+  }
+
+  nextAsSm(){
+    if(this.currentSlideAsSm + 1 === this.asianRestaurants.restaurants.length) return;
+      this.currentSlideAsSm = (this.currentSlideAsSm + 1) % this.asianRestaurants.restaurants.length;
+      
+      const offset = this.currentSlideAsSm * 315;
+
+      const myAnimation : AnimationFactory = this.builder.build([
+        animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+      ]);
+
+      this.playerAsSm = myAnimation.create(this.carouselAsSm.nativeElement);
+      this.playerAsSm.play();
+  }
+
+
+  prevAsSm(){
+    if(this.currentSlideAsSm + 1 === 0 || this.currentSlideAsSm === 0) return;
+    this.currentSlideAsSm = ((this.currentSlideAsSm - 1) % this.asianRestaurants.restaurants.length) % this.asianRestaurants.restaurants.length;
+    
+    const offset = this.currentSlideAsSm * 315;
+
+    const myAnimation : AnimationFactory = this.builder.build([
+      animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+    ]);
+
+    this.playerAsSm = myAnimation.create(this.carouselAsSm.nativeElement);
+    this.playerAsSm.play();
+  }
+
+
+  nextItSm(){
+    if(this.currentSlideItSm + 1 === this.italianRestaurants.restaurants.length) return;
+      this.currentSlideItSm = (this.currentSlideItSm + 1) % this.italianRestaurants.restaurants.length;
+      
+      const offset = this.currentSlideItSm * 315;
+
+      const myAnimation : AnimationFactory = this.builder.build([
+        animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+      ]);
+
+      this.playerItSm = myAnimation.create(this.carouselItSm.nativeElement);
+      this.playerItSm.play();
+  }
+
+
+  prevItSm(){
+    if(this.currentSlideItSm + 1 === 0 || this.currentSlideItSm === 0) return;
+    this.currentSlideItSm = ((this.currentSlideItSm - 1) % this.italianRestaurants.restaurants.length) % this.italianRestaurants.restaurants.length;
+    
+    const offset = this.currentSlideItSm * 315;
+
+    const myAnimation : AnimationFactory = this.builder.build([
+      animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+    ]);
+
+    this.playerItSm = myAnimation.create(this.carouselItSm.nativeElement);
+    this.playerItSm.play();
+  }
+
+  nextMexSm(){
+    if(this.currentSlideMexSm + 1 === this.mexSeaRestaurants.restaurants.length) return;
+      this.currentSlideMexSm = (this.currentSlideMexSm + 1) % this.mexSeaRestaurants.restaurants.length;
+      
+      const offset = this.currentSlideMexSm * 315;
+
+      const myAnimation : AnimationFactory = this.builder.build([
+        animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+      ]);
+
+      this.playerMexSm = myAnimation.create(this.carouselMexSm.nativeElement);
+      this.playerMexSm.play();
+  }
+
+
+  prevMexSm(){
+    if(this.currentSlideMexSm + 1 === 0 || this.currentSlideMexSm === 0) return;
+    this.currentSlideMexSm = ((this.currentSlideMexSm - 1) % this.mexSeaRestaurants.restaurants.length) % this.mexSeaRestaurants.restaurants.length;
+    
+    const offset = this.currentSlideMexSm * 315;
+
+    const myAnimation : AnimationFactory = this.builder.build([
+      animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+    ]);
+
+    this.playerMexSm = myAnimation.create(this.carouselMexSm.nativeElement);
+    this.playerMexSm.play();
+  }
+
+  nextHealthySm(){
+    if(this.currentSlideHealthySm + 1 === this.mexSeaRestaurants.restaurants.length) return;
+      this.currentSlideHealthySm = (this.currentSlideHealthySm + 1) % this.healthyRestaurants.restaurants.length;
+      
+      const offset = this.currentSlideHealthySm * 315;
+
+      const myAnimation : AnimationFactory = this.builder.build([
+        animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+      ]);
+
+      this.playerHealthySm = myAnimation.create(this.carouselHealthySm.nativeElement);
+      this.playerHealthySm.play();
+  }
+
+
+  prevHealthySm(){
+    if(this.currentSlideHealthySm + 1 === 0 || this.currentSlideHealthySm === 0) return;
+    this.currentSlideHealthySm = ((this.currentSlideHealthySm - 1) % this.healthyRestaurants.restaurants.length) % this.healthyRestaurants.restaurants.length;
+    
+    const offset = this.currentSlideHealthySm * 315;
+
+    const myAnimation : AnimationFactory = this.builder.build([
+      animate('350ms ease-in', style({transform: `translateX(-${offset}px)`}))
+    ]);
+
+    this.playerHealthySm = myAnimation.create(this.carouselHealthySm.nativeElement);
+    this.playerHealthySm.play();
   }
 
 }
